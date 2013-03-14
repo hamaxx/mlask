cf4ad0946985f97fe122ccd0b0dc356f85e9cd01
Jure Ham <jure@hamsworld.net>
2013-03-14 23:29:41 +0100

    cleaned empty settings

--- mlask/conf/empty_settings.py
+++ mlask/conf/empty_settings.py
@@ -7,20 +7,6 @@ SERVER = {
 	'workers': 1,
 }
 
-ADMINS = []
-LOG_FILE = 'mlask.log'
-GUNICORN_LOG_FILE = 'mlask_gunicorn.log'
-
-DATABASE = {
-	'engine': 'sqlite', # mysql, sqlite
-	'database': '',
-	'user': '',
-	'password': '',
-	'host': '',
-}
-DATABASE_TEST = {
-}
-
 RUNNING_TESTS = False
 
 MLASK_HOME_PATH = os.path.join(os.path.dirname(__file__), '../')
